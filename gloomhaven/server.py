93788cb2ff918b4c3a3b2aa46016b287b9cc0fa4
softyy <adkincj@gmail.com>
2019-09-08 15:39:06 -0400

    added a mobile viewport tag to meta tags

--- gloomhaven/server.py
+++ gloomhaven/server.py
@@ -8,6 +8,12 @@ from .templates.index import render as layout
 
 app = dash.Dash(__name__, external_stylesheets=[BOOTSTRAP])
 
+# Sets the html template fo the dash application
+file_path = os.path.dirname(os.path.realpath(__file__))
+index_path = os.path.join(file_path, 'templates', 'index.html')
+with open(index_path) as index_fs:
+    app.index_string = index_fs.read()
+
 app.layout = layout
 
 app.title = "Gloomhaven Campaign Manager"
